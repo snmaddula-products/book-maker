8f7893e7639cd610921ccbd1429bd9a2d502d7c9
snmaddula <snmaddula@outlook.com>
2019-02-21 03:27:23 +0530

    Initial Commit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  toppings = new FormControl();
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 }
---
+++ src/main/java/snmaddula/products/bookmaker/BookMakerApplication.java
@@ -0,0 +1,13 @@
+package snmaddula.products.bookmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMakerApplication.class, args);
+	}
+
+}
